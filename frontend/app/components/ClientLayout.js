800ef5cb0ce7ad247d9f67c2cea6150ad1e03f3e
kapil <kapil.kumar@elucidata.io>
2026-07-05 16:05:59 +0530

    fix frontend 1

--- frontend/app/components/ClientLayout.js
+++ frontend/app/components/ClientLayout.js
@@ -1,14 +1,19 @@
 "use client";
 
 import { AuthProvider } from "../lib/auth";
+import { CreditsProvider } from "../lib/credits";
 import AuthGuard from "./AuthGuard";
 import DialogHost from "./DialogHost";
+import CreditGuard from "./CreditGuard";
 
 export default function ClientLayout({ children }) {
   return (
     <AuthProvider>
-      <AuthGuard>{children}</AuthGuard>
-      <DialogHost />
+      <CreditsProvider>
+        <AuthGuard>{children}</AuthGuard>
+        <DialogHost />
+        <CreditGuard />
+      </CreditsProvider>
     </AuthProvider>
   );
 }
